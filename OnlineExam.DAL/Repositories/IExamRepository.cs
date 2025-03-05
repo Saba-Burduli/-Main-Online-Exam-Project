56ceb8398fdf4ee5c6df9cb1b744436783369089
Saba-Burduli <sabagg790@gmail.com>
2025-03-05 19:50:36 +0400

    git checking

---
+++ OnlineExam.DAL/Repositories/IExamRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnlineExam.DATA.Entites;
+using OnlineExam.DATA.Infrastructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.DAL.Repositories
+{
+    public interface IExamRepository :IBaseRepository<Exam>
+    {
+    }
+}
--- OnlineExam.SERVICE/ExamService.cs
+++ OnlineExam.SERVICE/ExamService.cs
@@ -1,71 +1,38 @@
 using Microsoft.EntityFrameworkCore;
+using OnlineExam.DAL.Repositories;
 using OnlineExam.DATA.Entites;
 using OnlineExam.SERVICE.DTOs.ExamModels;
 using OnlineExam.SERVICE.InterFaces;
+using System.Threading.Tasks;
 
 namespace OnlineExam.SERVICE;
 
 public class ExamService : IExamService
 {
-    //giorgis kodi
-    //private readonly
+    private readonly IUserRepository _userRepository;
+    public ExamService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
 
-    //public ExamService(TaskMenagerDbContext dbContext)
-    //{
-    //    _dbContext = dbContext;
-    //}
+    //[GET METHODS (START)]
+    public async Task<IEnumerable<Exam>> GetAllExams()
+    {
+        var exam =await _userRepository.getallexa
+    }
 
-    //public List<Exam> GetAllExams()
-    //{
-    //    return _dbContext.Exams.ToList();
-    //}
+    public Exam GetExamById(int examId)
+    {
+        throw new NotImplementedException();
+    }
 
-    //public Exam GetExamById(int id)
-    //{
-    //    return _dbContext.Exams.Find(id);
-    //}
+    public IEnumerable<Exam> GetExamsByTeacher(int teacherId)
+    {
+        throw new NotImplementedException();
+    }
 
-    //public void AddExam(Exam exam)
-    //{
-    //    _dbContext.Exams.Add(exam);
-    //    _dbContext.SaveChanges();
-    //}
+    //[GET METHODS (END)]
 
-    //public void UpdateExam(Exam exam)
-    //{
-    //    _dbContext.Entry(exam).State = EntityState.Modified;
-    //    _dbContext.SaveChanges();
-    //}
-
-    //public void DeleteExam(int id)
-    //{
-    //    var exam = _dbContext.Exams.Find(id);
-    //    if (exam != null)
-    //    {
-    //        _dbContext.Exams.Remove(exam);
-    //        _dbContext.SaveChanges();
-    //    }
-    //}
-
-    //public List<Exam> GetExamsBySubject(string subject)
-    //{
-    //    return _dbContext.Exams.Where(e => e.Subject == subject).ToList();
-    //}
-
-    //public List<Exam> GetExamsByGrade(string grade)
-    //{
-    //    return _dbContext.Exams.Where(e => e.Grade == grade).ToList();
-    //}
-
-    //public Exam GetExamByCode(string code)
-    //{
-    //    return _dbContext.Exams.FirstOrDefault(e => e.Code == code);
-    //}
-
-    //public bool IsExamExists(int id)
-    //{
-    //    return _dbContext.Exams.Any(e => e.Id == id);
-    //}
     public void AddQuestion(int examId, Question question)
     {
         throw new NotImplementedException();
@@ -77,21 +44,6 @@ public class ExamService : IExamService
     }
 
     public void DeleteExam(int examId)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IEnumerable<Exam> GetAllExams()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Exam GetExamById(int examId)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IEnumerable<Exam> GetExamsByTeacher(int teacherId)
     {
         throw new NotImplementedException();
     }
