d3a73d96858e6a26ca4a7f22168d0267d756fc02
Saba-Burduli <sabagg790@gmail.com>
2025-03-10 22:12:29 +0400

    another commit

--- OnlineExam.DATA/Entites/Question.cs
+++ OnlineExam.DATA/Entites/Question.cs
@@ -7,17 +7,21 @@ public class Question
 {
     [Key]
     public int QuestionId { get; set; }
+
     [MaxLength(200,ErrorMessage = "content length cannot exceed 200 characters.")]
     [Required(ErrorMessage = "content is required")]
     public string? Context { get; set; }
+
     [Required(ErrorMessage = "content is required")]
     public string? CorrectAnswer { get; set; }
     
     //Relations
+
     //Question => Exam ; Many to One ; (Question) to (Exam)
     [ForeignKey("Exam")] 
     public int ExamId { get; set; }
     public virtual Exam? Exam { get; set; }
+
     //Question => Options
     public virtual ICollection<Option>? Options { get; set; }
     
--- OnlineExam.DATA/Entites/Role.cs
+++ OnlineExam.DATA/Entites/Role.cs
@@ -7,9 +7,11 @@ public class Role
 {
     [Key]
     public int RoleId { get; set; }
+
     [MaxLength(30,ErrorMessage = "Role name cannot be longer than 30 characters.")]
     [Required(ErrorMessage = "Role name is required")]
     public string? RoleName { get; set; }
+
     //Relations :
     //Role => User ; Many to Many
     public ICollection<User>? Users { get; set; }
