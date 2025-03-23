42b14e2cf24535447a770124dc5d16e0a0e23ef1
Saba-Burduli <sabagg790@gmail.com>
2025-03-23 22:22:43 +0400

    Adding Question Model fields

--- OnlineExam.SERVICE/DTOs/QuestionModels/QuestionModel.cs
+++ OnlineExam.SERVICE/DTOs/QuestionModels/QuestionModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using OnlineExam.DATA.Entites;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,10 @@ namespace OnlineExam.SERVICE.DTOs.QuestionModels
 {
     public class QuestionModel
     {
+        public int QuestionId { get; set; }
+        public string? Context { get; set; }
+        public string? CorrectAnswer { get; set; }
+        public int ExamId { get; set; }
+        
     }
 }
