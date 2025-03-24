7b7b24b1da04753134c28e372af2d2d322679444
Saba-Burduli <sabagg790@gmail.com>
2025-03-24 21:29:22 +0400

    adding some methods in IQuestion Service

--- OnlineExam.SERVICE/InterFaces/IQuestionService.cs
+++ OnlineExam.SERVICE/InterFaces/IQuestionService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlineExam.DATA.Entites;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace OnlineExam.SERVICE.InterFaces
 {
     public interface IQuestionService
     {
+        Task<Question> AddQuestion(Question question);
+    }
+
+    public class QuestionService : IQuestionService
+    {
+        public Task<Question> AddQuestion(Question question)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
